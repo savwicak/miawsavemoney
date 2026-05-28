e077d2e80c22291f0f66767378af56a6cdba1a01
sapu <savwicak@gmail.com>
2026-05-28 23:12:29 +0700

    switch scene

--- scenes/props/portal.gd
+++ scenes/props/portal.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @export var required_fireflies := 20
-@export var next_scene := "res://scenes/mainmenu.tcsn"
 
 func _ready() -> void:
 	visible = false
@@ -17,6 +16,12 @@ func _process(delta: float) -> void:
 			visible = false
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file(next_scene)
+		print("PINDAH SCENE")
+
+		call_deferred("go_to_next_scene")
+
+
+func go_to_next_scene():
+	get_tree().change_scene_to_file("res://scenes/start_game.tscn")
